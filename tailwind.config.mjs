78aba982d18bb4063ad3d7ccc279731c899da239
elue-dev <eluewisdom@gmail.com>
2024-10-20 00:31:20 +0100

    fix: errors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 import starlightPlugin from "@astrojs/starlight-tailwind";
-import colors from "tailwindcss/colors";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
-        accent: "#000",
-        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-        gray: "#000",
-      },
-    },
+    extend: {},
   },
   plugins: [starlightPlugin()],
 };
