2c6765d4154fba9c528a58504775951801379d1d
elue-dev <eluewisdom@gmail.com>
2024-10-21 19:27:33 +0100

    patch: font changes

--- src/config/expressive-code-options.ts
+++ src/config/expressive-code-options.ts
@@ -2,7 +2,7 @@ export const EXPRESSIVE_CODE_OPTIONS = {
   themes: ["vitesse-dark", "vitesse-light"],
   styleOverrides: {
     borderRadius: "0.2rem",
-    codeFontFamily: "Outfit, sans-serif",
+    codeFontFamily: "Inter, sans-serif",
     codeFontSize: "0.95rem",
     frames: {
       shadowColor: "none",
