77bce227efa75e56e11e283546db8f1e043dfb91
elue-dev <eluewisdom@gmail.com>
2024-10-24 08:35:31 +0100

    feat: add roobert font

--- src/config/expressive-code-options.ts
+++ src/config/expressive-code-options.ts
@@ -2,7 +2,8 @@ export const EXPRESSIVE_CODE_OPTIONS = {
   themes: ["vitesse-dark", "vitesse-light"],
   styleOverrides: {
     borderRadius: "0.2rem",
-    codeFontFamily: "Outfit, sans-serif",
+    codeFontFamily: "Roobert, sans-serif;",
+
     codeFontSize: "0.95rem",
     frames: {
       shadowColor: "none",
